bcd0615bdab81ebc76de9a54e6052bb90b51e691
Panth112802 <36990771+Panth112802@users.noreply.github.com>
2019-05-30 22:06:00 +0200

    testDentistDAo

---
+++ dentalclinic/src/main/java/DAO/DentistDAO.java
@@ -0,0 +1,112 @@
+package DAO;
+
+public class DentistDAO implements IDentistDAO {
+	
+	private DatabaseConnection dbconn;
+
+	public DentistDAO() {
+
+		dbconn = new DatabaseConnection();
+
+	}
+
+	public Dentist getDentist(int id) {
+		Dentist tempDentist = null;
+		String sql = "select * from \"DentalClinic\".dentist where dentistid=" + id + ";";
+		
+		ResultSet rs = dbconn.retriveData(sql);
+		try {
+			while (rs.next()) {
+				int dentistId = rs.getInt("dentistid");
+				String firstName = rs.getString("FirstName");
+				String lastName = rs.getString("lastName");
+				int branchId = rs.getInt("branchid");
+				String Speciality= rs.getString("specialist");
+				
+			
+				tempDentist = (new Dentist(dentistId,firstName, lastName,branchId , Speciality));
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println(tempDentist.getBranchId());
+		
+		
+		return tempDentist;
+	}
+
+	public List<Dentist> getAllDentist()  {
+		List<Dentist> dentistList = new ArrayList<Dentist>();
+		String sql = "SELECT * FROM \"DentalClinic\".dentist;";
+
+		ResultSet rs = dbconn.retriveData(sql);
+		
+		try {
+			while (rs.next()) {
+				int dentistId = rs.getInt("dentistid");
+				String firstName = rs.getString("FirstName");
+				String lastName = rs.getString("lastName");
+				int branchId = rs.getInt("branchid");
+				String speciality= rs.getString("specialist");
+				
+				dentistList.add( new Dentist(dentistId, firstName, lastName, branchId, speciality));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println(dentistList.size());
+		return dentistList;
+	}
+	public List <Dentist> getAllDentistByBranchId(int branchId) {
+		
+	  List<Dentist> dentistListByBranchId = new ArrayList<Dentist>();
+		String sql = "SELECT * FROM \"DentalClinic\".dentist WHERE branchid =" + branchId +";";
+		ResultSet rs = dbconn.retriveData(sql);
+		
+		try  {
+			while(rs.next()) {
+				int dentistId = rs.getInt("dentistid");
+				String firstName = rs.getString("FirstName");
+				String lastName = rs.getString("lastName");
+				String Speciality= rs.getString("specialist");
+				
+				dentistListByBranchId.add(new Dentist(dentistId, firstName, lastName,branchId , Speciality));
+				
+			}
+		}catch(SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return dentistListByBranchId;
+	}
+	
+	public void AddDentist(Dentist newDentist) {
+
+		String sql = "INSERT INTO \"DentalClinic\".dentist VALUES(" + newDentist.getEmpNo() + ",'" + newDentist.getFirstName()
+				+ "','" + newDentist.getLastName() + "',"+ newDentist.getBranchId()+ ",'" + newDentist.getSpecialty() + "');";
+		dbconn.WriteData(sql);
+
+	}
+
+	@Override
+	public void RemoveDentist(int dentistId) {
+		String sql = "Delete from \"DentalClinic\".dentist where dentistid ="+ dentistId + ";";
+		
+		dbconn.WriteData(sql);
+		
+	}
+
+	@Override
+	public void EditDentist(Dentist dentist) {
+		String sql = "UPDATE \"DentalClinic\".dentist "
+				+ " SET firstname ='"+dentist.getFirstName()+"', lastname ='"+dentist.getLastName()+"', branchid='" +dentist.getBranchId() +"', specialist ='"+dentist.getSpecialty()+"'"
+						+ "Where dentistid ="+dentist.getEmpNo()+";";
+				
+		dbconn.WriteData(sql);
+		
+		// TODO Auto-generated method stub
+		
+	}
+}
