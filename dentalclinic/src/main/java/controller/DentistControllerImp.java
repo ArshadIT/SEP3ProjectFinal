9debf026531510baff3a742b442227f5c353a74a
Panth112802 <36990771+Panth112802@users.noreply.github.com>
2019-06-01 15:34:32 +0200

    editdentist

--- dentalclinic/src/main/java/controller/DentistControllerImp.java
+++ dentalclinic/src/main/java/controller/DentistControllerImp.java
@@ -7,35 +7,42 @@ import DAO.IDentistDAO;
 import model.Dentist;
 
 public class DentistControllerImp implements IDentistController   {
-private IDentistDAO dentist;
+private IDentistDAO dentistDAO = new DentistDAO();
 	public DentistControllerImp() {
-		dentist = new DentistDAO();
+		
 	}
+		
 
 	@Override
 	public void AddDentist(Dentist newDentist) {
-		dentist.AddDentist(newDentist);
+		dentistDAO.AddDentist(newDentist);
 	}
 
 	@Override
 	public Dentist getDentistByDentistId(int Id) {
 
-		return dentist.getDentist(Id);
+		return dentistDAO.getDentist(Id);
 	}
 
 	@Override
 	public List<Dentist> getAllDentistByBranchId(int branchId) {
-		return  dentist.getAllDentistByBranchId(branchId);
+		return  dentistDAO.getAllDentistByBranchId(branchId);
 	}
 
 	@Override
 	public List<Dentist> getAllDentist() {
-		return dentist.getAllDentist();
+		return dentistDAO.getAllDentist();
 	}
 
 	@Override
 	public void RemoveDentistById(int id) {
-		dentist.RemoveDentist(id);
+		dentistDAO.RemoveDentist(id);
+	}
+
+
+	@Override
+	public void EditDentist(Dentist dentist) {
+		dentistDAO.EditDentist(dentist);
 	}
 
 }
